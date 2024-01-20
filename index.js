2674ecd20860ea77c0a2463a3af806e5f4187408
MEA-23 <77838294+MEA-23@users.noreply.github.com>
2024-01-20 15:11:51 +0200

    create library

---
+++ index.js
@@ -0,0 +1,8 @@
+// Loaded by: node_modules/mea/package.json
+// import path from "path";
+
+// const home = path.resolve("..", "..", "core", "main.js");
+
+// console.log(home);
+
+export * from "../../core/main.js";
